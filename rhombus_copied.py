d1590e2b7cbddac6bb7090a43abfed05a3ca37b0
Rubina Karki <rubinakarki987@gmail.com>
2018-03-04 16:55:29 +0545

    second commit

---
+++ rhombus_copied.py
@@ -0,0 +1,29 @@
+import turtle
+def box(argument):
+	argument.right(75)
+	argument.forward(100)
+	argument.right(45)		
+	argument.forward(100)
+	argument.right(135)
+	argument.forward(100)
+	argument.right(45)
+	argument.forward(100)
+
+
+def square():
+
+	window=turtle.Screen()
+	window.bgcolor('blue')
+	window.title('what is this?')
+
+	brad = turtle.Turtle()
+	brad.color('yellow')
+	brad.shape('classic')
+	brad.speed(100)
+	for i in range(1,80):
+		box(brad)
+		brad.right(5)
+
+	window.exitonclick()
+	
+square()
