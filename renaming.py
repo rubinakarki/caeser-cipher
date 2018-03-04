6e362b77acd2e775ba618c5797218ddbe9c15aa4
Rubina Karki <rubinakarki987@gmail.com>
2018-03-04 17:16:37 +0545

    this is the second changes made in the first file

--- renaming.py
+++ renaming.py
@@ -1,17 +1,2 @@
 import os 
 import string 
-def renameFiles():
-	file = os.listdir(r"C:\prank")#list the item in the directory in the given path
-	#print (file)
-	r= os.getcwd()#current working directory
-	print("current working directory is :"+ r) 
-	os.chdir(r"C:\prank")#change the directory path
-
-	for f in file:
-		translation_table= str.maketrans(" "," ","0123456789")#table consists of mapping of first 
-																#and second string or anything such as ("abc","ghi","bc=delete character")
-															  #a-g,b-h,c-i,,,,,,string=abcdef------>ghi]def
-		os.rename(f,f.translate(translation_table))			
-	os.chdir(r) 
-	
-renameFiles()
